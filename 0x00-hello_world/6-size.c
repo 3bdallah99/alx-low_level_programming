5529f9c7c0215dd131d26da3b713eee96a654d74
Abdallah <abdallahanwar899@gmail.com>
2023-08-14 10:15:37 -0700

    boda

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int i;
 long int l;
 long long int ll;
 float f;
-printf("Size of a char: %lu bytes.\n", sizeof(c));
-printf("Size of an int: %lu bytes.\n", sizeof(i));
-printf("Size of a long int: %lu bytes.\n", sizeof(l));
-printf("Size of a long long int: %lu bytes.\n", sizeof(ll));
-printf("Size of a float: %lu bytes.\n", sizeof(f));
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 return (0);
 }
