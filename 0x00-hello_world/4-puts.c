12fd22926bc0093cb9f8a1f24182df254bcbabcf
Abdallah <abdallahanwar899@gmail.com>
2023-08-14 01:25:42 -0700

    kosom betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,13 @@
 /**
  * main - this is main func in the app
  *
+ * description: A c program that print with puts
+ *
+ * Return: always 0 (success)
+ *
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
