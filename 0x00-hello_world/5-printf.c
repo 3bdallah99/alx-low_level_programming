5c7924dc7014094562d70c3f27761a5c12f42646
Abdallah <abdallahanwar899@gmail.com>
2023-08-14 01:36:42 -0700

    kosom betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-int main() {
-      
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+/**
+ * main - main func print with printf
+ * Decription: print this phrese to do this fucken task
+ * Return: return 0 every
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
 
