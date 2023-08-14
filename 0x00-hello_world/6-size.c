f236f164499e8a7e90872dd19e1fcf341e4aaf5b
Abdallah <abdallahanwar899@gmail.com>
2023-08-14 10:13:25 -0700

    boda

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,21 +7,15 @@
 int main(void)
 {
 char c;
-short int s;
 int i;
 long int l;
 long long int ll;
 float f;
-double d;
-long double ld;
-printf("The size of char is %lu bytes.\n", sizeof(c));
-printf("The size of short int is %lu bytes.\n", sizeof(s));
-printf("The size of int is %lu bytes.\n", sizeof(i));
-printf("The size of long int is %lu bytes.\n", sizeof(l));
-printf("The size of long long int is %lu bytes.\n", sizeof(ll));
-printf("The size of float is %lu bytes.\n", sizeof(f));
-printf("The size of double is %lu bytes.\n", sizeof(d));
-printf("The size of long double is %lu bytes.\n", sizeof(ld));
+printf("Size of a char: %lu bytes.\n", sizeof(c));
+printf("Size of an int: %lu bytes.\n", sizeof(i));
+printf("Size of a long int: %lu bytes.\n", sizeof(l));
+printf("Size of a long long int: %lu bytes.\n", sizeof(ll));
+printf("Size of a float: %lu bytes.\n", sizeof(f));
 
 return (0);
 }
