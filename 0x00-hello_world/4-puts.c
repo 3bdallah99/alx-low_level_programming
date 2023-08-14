780ec16173920f168cea452ea0a1855dda3eadc7
Abdallah <abdallahanwar899@gmail.com>
2023-08-14 01:05:57 -0700

    dodo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - this is main func in the app
+ *
+ */
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
-
