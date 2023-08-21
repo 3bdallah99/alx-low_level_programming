aa782967f858fac97aa3a0d0a14c1a833e261e00
Abdallah <abdallahanwar899@gmail.com>
2023-08-20 18:19:58 -0700

    boda

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * 1-isdigit.c - to check for a digit (0 through 9)
- * Description - boda
+ * @c: input char alphapet
  * Return - 1 or 0
- */
-int _isdigit(int c) {
-if (c >= '0' && c <= '9')
+*/
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
 return (1);
 else
 return (0);
