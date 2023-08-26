af6e46f90f790c95b5889e6057b2ad89e4d68d8f
Abdallah <abdallahanwar899@gmail.com>
2023-08-26 04:57:38 -0700

    boda

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;  
+for (i = 0; i <= 90; i+10)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
