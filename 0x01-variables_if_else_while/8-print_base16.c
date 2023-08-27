e594f996c51721ceca3a24afb48cd7360982a93d
Abdallah <abdallahanwar899@gmail.com>
2023-08-26 20:25:27 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase
+ *
+ * Description - Boda on fire
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letter;
+for (letter = 97; letter <= 112; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
