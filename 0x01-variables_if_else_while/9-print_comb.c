428b2ecfa59cd19ea69f867355ad03909c619c60
Abdallah <abdallahanwar899@gmail.com>
2023-08-26 20:41:02 -0700

    nb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+puthcar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
