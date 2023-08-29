4649dc3bd589572223d581bd4910af9cfe4893a9
Abdallah <abdallahanwar899@gmail.com>
2023-08-29 08:28:43 -0700

    lastcommit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * _strstr - a function that locates a substring
- * @haystack: input
- * @needle: input
- * Return: 0 (success)
- */
+ * print_chessboard - function that prints the chessboard.
+ * @a: the array
+*/
 void print_chessboard(char (*a)[8])
 {
 int x = 0;
