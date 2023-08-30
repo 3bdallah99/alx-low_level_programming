3c4f13f8cef84ae8ef0f6977f496dda2f4af4ed8
Abdallah <abdallahanwar899@gmail.com>
2023-08-29 18:28:08 -0700

    boood

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absol
+ * @n: takes in integer type input for function
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int n)
+{
+return ((n > 0) ? n : -n);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Always 0 (Success)
+*/
+
+void times_table(void)
+{
+int h;
+int i;
+int j;
+for (i = 0; i < 10; ++i)
+{
+_putchar(48);
+for (j = 1; j < 10; ++j)
+{
+_putchar(',');
+_putchar(' ');
+h = i * j;
+if (h <= 9)
+_putchar(' ');
+else
+_putchar((h / 10) + 48);
+_putchar((h % 10) + 48);
+}
+_putchar('\n');
+}
+}
