9ebe589d0d86c294e79f34011bc5ed545443da60
Abdallah <abdallahanwar899@gmail.com>
2023-09-04 04:16:31 -0700

    well donw

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
-include < stdio.h >
+#include <stdio.h>
+#include "main.h"
 /**
  * main - prints its name, followed by a new line.
- * description -  it will print the new name,
- * without having to compile it again
- * return: string name
+ * @argc: number of command line arguments
+ * @argv: array
+ *
+ * Return: 0
  */
-int main(int argc, string argv[])
+int main(int argc, char *argv[])
 {
+(void)argc;
 printf("%s\n", argv[0]);
 return (0);
 }
