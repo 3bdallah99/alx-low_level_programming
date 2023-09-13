6bf75ff515bde24b24426578114083efbcdb0cc9
root <root@BODA.localdomain>
2023-09-14 01:32:28 +0300

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - to point to function boda anwar
+ * @name: pointer has type char
+ * @f: void point
+ * return: NONE
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
